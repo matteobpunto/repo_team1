575b541a9b19d396e0e7f84646d64db5059a24eb
emkey212 <mirykarim1@gmail.com>
2025-02-20 14:32:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def user_choice(): #Chiede all'utente di scegliere un'opzione
 def computer_choice():
     return random.choice(options) #Genera casualmente una scelta per il computer
 
-def tie_condition(user, computer): #Determina il risultato del gioco confrontando le scelte dell'utente e del computer
-    if user == computer:
+def winning_condition(user, computer): #Determina il risultato del gioco confrontando le scelte dell'utente e del computer
+    if user == computer: #Caso di pareggio
         return "It's a tie! (˶°ㅁ°)!!"
     elif (user == "rock" and computer == "scissors") or (user == "paper" and computer == "rock") or (user == "scissors" and computer == "paper"):
         return "You won! ദ്ദി •⩊• )"
@@ -25,5 +25,5 @@ user = user_choice()
 computer = computer_choice()
 print(f"User choice: {user}")
 print(f"Computer choice: {computer}")
-result = tie_condition(user, computer)
+result = winning_condition(user, computer)
 print(result)
